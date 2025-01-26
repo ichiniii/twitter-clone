3f7727721d2bbf599e6677770fe49b9bc1025af2
ichiniii <ichiniii>
2025-01-27 05:53:47 +0900

    Updates

--- dataconnect-generated/js/default-connector/esm/index.esm.js
+++
@@ -1,8 +0,0 @@
-import { getDataConnect, validateArgs } from 'firebase/data-connect';
-
-export const connectorConfig = {
-  connector: 'default',
-  service: 'twitter-clone-udemy',
-  location: 'us-central1'
-};
-
--- dataconnect-generated/js/default-connector/index.cjs.js
+++
@@ -1,9 +0,0 @@
-const { getDataConnect, validateArgs } = require('firebase/data-connect');
-
-const connectorConfig = {
-  connector: 'default',
-  service: 'twitter-clone-udemy',
-  location: 'us-central1'
-};
-exports.connectorConfig = connectorConfig;
-
